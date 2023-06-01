6dc3d0fc54ca2e0ee548c65b3aee5d4473c14502
Philipp Maier <git@pmaier.at>
2023-06-01 16:10:28 +0200

    Add tuples

--- 2_list_types/tuples/main.tf
+++ 2_list_types/tuples/main.tf
@@ -1,8 +1,27 @@
-# Note that type `list` is ordered and can be referenced with [0], [1], etc..
-# variable "tuple" {
-#   default = [1, "2", false]
-#   type    = tuple(number, string, bool)
-# }
+# tuple is defined as `tuple([type_of_element_1, type_of_element_2, ...])`
+# If the type for an element is unknown `any` can be used. It will transform into the best suited type
+variable "tuple" {
+  default = ["1", 2, "3", false, "2"]
+  type    = tuple([string, number, string, bool, string])
+}
 
+# Will result in type tuple([string])
+variable "tuple_any" {
+  default = ["1"]
+  type    = tuple([any])
+}
 
-# Only one who can have different types compared to list or set
+locals {
+  # Note that this list is going to be a tuple
+  tuple_of_numbers = [1, 10, 100]
+
+  # Note that this is commented out because it produces an error
+  # max_number           = max(local.tuple_of_numbers)
+  max_number_corrected = max(local.tuple_of_numbers...)
+
+  # List Comprehension to show list identifiers
+  list_comprehension = [for i, o in var.tuple : "Element ${o} is at identifier value ${i}"]
+
+  # Advanced use of List Comprehension
+  list_comprehension_advanced = [for i, o in var.tuple : "Element ${upper(o)} is at identifier value ${i}" if can(regex("[^\\d]", o))]
+}
